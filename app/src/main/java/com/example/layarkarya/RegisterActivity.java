61166faaa885e8b677d9217d627e096c5c39e14c
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2022-10-10 19:45:05 +0700

    init client app

---
+++ app/src/main/java/com/example/layarkarya/RegisterActivity.java
@@ -0,0 +1,89 @@
+package com.example.layarkarya;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    public TextInputEditText etRegEmail;
+    public TextInputEditText etRegPassword;
+    public TextView tvLoginHere;
+    public CardView btnRegister;
+
+    public FirebaseAuth mAuth;
+    public TextView toolbarTitle;
+    public ImageView toolbarBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etRegEmail = findViewById(R.id.etRegEmail);
+        etRegPassword = findViewById(R.id.etRegPass);
+        tvLoginHere = findViewById(R.id.tvLoginHere);
+        btnRegister = findViewById(R.id.btnRegister);
+        toolbarTitle = findViewById(R.id.toolbar_title);
+        toolbarBtn = findViewById(R.id.toolbar_back_arrow);
+
+        toolbarTitle.setText(getResources().getString(R.string.register));
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(view ->{
+            createUser();
+        });
+
+        tvLoginHere.setOnClickListener(view ->{
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+        });
+
+        toolbarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void createUser(){
+        String email = etRegEmail.getText().toString();
+        String password = etRegPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            etRegEmail.setError("Email cannot be empty");
+            etRegEmail.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            etRegPassword.setError("Password cannot be empty");
+            etRegPassword.requestFocus();
+        }else{
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+}

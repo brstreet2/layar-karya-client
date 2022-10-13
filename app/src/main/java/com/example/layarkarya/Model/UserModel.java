3ba38856d0882080a7caeaffe224153aaa198858
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2022-10-13 09:02:07 +0700

    first name last name field, new model

---
+++ app/src/main/java/com/example/layarkarya/Model/UserModel.java
@@ -0,0 +1,16 @@
+package com.example.layarkarya.Model;
+
+public class UserModel {
+    public String fullName, lastName, email;
+
+    public UserModel(){
+
+    }
+
+    public UserModel(String fullName, String lastName, String email){
+        this.fullName = fullName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+}
--- app/src/main/java/com/example/layarkarya/RegisterActivity.java
+++ app/src/main/java/com/example/layarkarya/RegisterActivity.java
@@ -12,22 +12,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.layarkarya.Model.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
     public TextInputEditText etRegEmail;
     public TextInputEditText etRegPassword;
+    public TextInputEditText etFirstName;
+    public TextInputEditText etLastName;
     public TextView tvLoginHere;
     public CardView btnRegister;
 
     public FirebaseAuth mAuth;
     public TextView toolbarTitle;
     public ImageView toolbarBtn;
+    public DatabaseReference dbReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,8 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         etRegEmail = findViewById(R.id.etRegEmail);
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
         etRegPassword = findViewById(R.id.etRegPass);
         tvLoginHere = findViewById(R.id.tvLoginHere);
         btnRegister = findViewById(R.id.btnRegister);
@@ -63,21 +71,44 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void createUser(){
         String email = etRegEmail.getText().toString();
+        String firstName = etFirstName.getText().toString();
+        String lastName = etLastName.getText().toString();
         String password = etRegPassword.getText().toString();
 
         if (TextUtils.isEmpty(email)){
-            etRegEmail.setError("Email cannot be empty");
+            etRegEmail.setError("Email cannot be empty!");
             etRegEmail.requestFocus();
-        }else if (TextUtils.isEmpty(password)){
-            etRegPassword.setError("Password cannot be empty");
+        }
+        if (firstName.isEmpty()){
+            etFirstName.setError("Enter your first name!");
+            etFirstName.requestFocus();
+        }
+        if(lastName.isEmpty()){
+            etLastName.setError("Enter your last name!");
+            etLastName.requestFocus();
+        }
+        else if (TextUtils.isEmpty(password)){
+            etRegPassword.setError("Password cannot be empty!");
             etRegPassword.requestFocus();
         }else{
             mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
-                        Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                        UserModel user = new UserModel(firstName, lastName, email);
+                        FirebaseDatabase.getInstance("https://layarkarya-65957-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                        }
+                                        else{
+                                            Toast.makeText(RegisterActivity.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
                     }else{
                         Toast.makeText(RegisterActivity.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
